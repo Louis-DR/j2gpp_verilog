deb197add1a5f86995fbf87605b2e1b5b810e8b9
Louis Duret-Robert <louisduret@gmail.com>
2026-02-10 22:11:27 +0100

    [Feature] Add the 'array_width' filter with 'arr' alias

--- j2gpp_verilog/filters.py
+++ j2gpp_verilog/filters.py
@@ -12,6 +12,7 @@
 
 
 from math import ceil, log2
+from j2gpp.utils import throw_error
 
 
 
@@ -19,4 +20,28 @@ extra_filters = {}
 
 
 
+# ┌───────────────┐
+# │ Basic filters │
+# └───────────────┘
+
 extra_filters['clog2'] = lambda x : ceil(log2(x))
+
+# Vector or array declaration size
+def array_width(width, str_len=0):
+  if width is None: return "".rjust(str_len)
+  elif isinstance(width, str):
+    if width == "": return "".rjust(str_len)
+    else: return f"[{width}-1:0]".rjust(str_len)
+  else:
+    try:
+      width = int(width)
+    except Exception as exc:
+      throw_error(f"Exception occured in filter 'array_width' width width '{width}' : \n  {type(exc).__name__}\n{intend_text(exc)}")
+      return ""
+    if width <= 0:
+      throw_error(f"Invalid zero or negative width '{width}' for filter 'array_width'.")
+      return ""
+    elif width == 1: return "".rjust(str_len)
+    else: return f"[{width-1}:0]".rjust(str_len)
+extra_filters['array_width'] = array_width
+extra_filters['arr']         = array_width # Alias
